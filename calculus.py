53c119edb6fb37d9c3106f789f9b3b97e55a9aa4
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2016-11-07 15:53:41 -0500

    Added example to documentation

--- calculus.py
+++ calculus.py
@@ -10,6 +10,12 @@ def euler(dy_dx = lambda x,y: None, h = 1e-6, y0 = 1, start = 0, end = 1):
     y0: y value when x is start
     start: first x value
     end: x value to approximate
+
+    Example:
+    # Evaluate x**2 at 1 using Euler knowing only derivative and that 0**2 = 0
+    y_vals = euler(lambda x, y: 2 * x, start = 0, y0 = 0, end = 1)
+    print("1^2: %f euler pred: %f difference: %f" % (1**2, y_vals[-1], float(1**2) - y_vals[-1]))
+    >>> 1^2: 1.000000 euler pred: 1.000001 difference: -0.000001
     """
 
     nsteps = float(end - start) / h
