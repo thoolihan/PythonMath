cd81a07f66fa6be5696263a9d38b29acb4c28344
Tim Hoolihan <tim@hoolihan.net>
2016-11-11 19:05:05 -0500

    Add example if you run visualize_calculus directly

--- visualize_calculus.py
+++ visualize_calculus.py
@@ -19,3 +19,8 @@ def v_newton(iter=10, x0=0, fx=lambda x: x, dx=lambda x: x, width=5.):
         plt.annotate(lbl, xy=(x,y), xytext = (-10, 10),
                      textcoords='offset points', alpha=.5)
     plt.show()
+
+
+if __name__ == "__main__":
+    v_newton(5, 3, lambda x: x**2, lambda x: 2*x)
+    input('Press any key to exit...')
